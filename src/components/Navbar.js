740579cf432d164b3d1485e8a0f3644845896e5d
MAHEEN7126 <maheenbano252@gmail.com>
2025-07-16 12:18:35 +0500

    Fix layout for Simora Todo text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <nav className=" relative bg-gradient-to-r from-[#e4a5ffaa] via-[#deabff66] to-[#b8d0ffdd] backdrop-blur-md shadow-md border-b border-white/20 px-4 py-4">
-      <div className="max-w-7xl mx-auto  relative flex  items-center">
-        <p className="text-xl font-bold text-puple-200 "> 🌟 Simora Todo</p>
+      <div className="max-w-7xl mx-auto  relative flex  items-center space-x-2">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl font-bold text-black "> 🌟 Simora Todo</p>
         <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-8 ">
           <Link
             to="/"
